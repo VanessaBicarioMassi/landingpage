afb94206fc4fb66ad6fe9080cb4b11e2d8099192
fdemarque <demarque.ads@gmail.com>
2023-05-24 22:17:25 -0300

    muita coisa

---
+++ JS/botaoInterativo.js
@@ -0,0 +1,7 @@
+// Obtém o elemento do botão
+var button = document.querySelector('.capsula');
+var imagem = document.querySelector('.gato-fofo');
+
+button.addEventListener('mouseover', function() {
+  imagem.style.display = 'block';
+});
